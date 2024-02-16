b62c71bf549033a97b36d6e563911c7bea9a3917
Кирилл Капитальцев <kkapitaltsev@alfabank.ru>
2024-02-16 17:23:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const num = 23;
+
+const str = 'string';
+
+const bool = true;
+
+const obj = {
+    age: 29,
+    name: 'Кирилл',
+    isAdmin: true,
+};
+
+const msg1 = 'msg1';
+const msg2 = 'msg2';
+
+const errorFn = () => {};
+const fixMsg = "fix_msg";
+const oneMoreMsg = "one_more_message";
+const h = 22;
+
+const newMsg = 'new_msg';
+const newMsg2 = 'new_msg_2';
+
