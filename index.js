0eb31f51535a620fd414b1a398ea083b2b3110d3
Кирилл Капитальцев <kkapitaltsev@alfabank.ru>
2024-02-26 12:01:25 +0300

    add msg2
    add msg on main
    
    initial commit
    
    add msg3

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const num = 23;
 
 const str = 'string';
 
-const bool = false;
+const bool = true;
 
-const msg = 'msg1';
+const obj = {
+    age: 29,
+    name: 'Кирилл',
+    isAdmin: true,
+};
+
+const msg1 = 'msg1';
+const msg2 = 'msg2';
+const msg3 = 'msg3';
+
+const errorFn = () => {};
+const fixMsg = "fix_msg";
+const oneMoreMsg = "one_more_message";
+const h = 22;
+
+const newMsg = 'new_msg';
+const newMsg2 = 'new_msg_2';
+
